2a90a85e4a4d86d77db92464c183619401358352
Nick <nicholas.strazicic2027@gbwl.org>
2026-05-13 15:28:52 +0000

    added buttons

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const body = document.querySelector("body");
 const heading = document.querySelector("#main-heading");
 const description = document.querySelector("#description");
 const toggleBtn = document.querySelector("#toggle");
+const image = document.querySelector("#image");
 
 // JS can now change attributes & properties
 heading.textContent = "Light/Dark Mode Website"; // JS wrote HTML code!
@@ -34,6 +35,7 @@ function toggleMode() {
         toggleBtn.style.background = "darkgrey";
         description.textContent = "Welcome to the Dark Side";
         toggleBtn.textContent = "Switch to Light";
+        image.src = "Darth_vader_by_br3ndan5_dcvp5rb.webp";
     }
     else {
         console.log("apply light mode");
@@ -44,9 +46,46 @@ function toggleMode() {
         toggleBtn.style.background = "lightgrey";
         description.textContent = "Welcome to the Light Side";
         toggleBtn.textContent = "Switch to Dark";
+        image.src = "Anakin_Skywalker_RotS.webp";
     }
 }
 
 // Attach function to the button
 toggleBtn.addEventListener("click", toggleMode);
+
+
+
+// REVIEW
+// 1. Select an HTML element for JS to target
+const hackerBtn = document.querySelector("#hacker");
+// 2. Attach click event listener to trigger a function
+hackerBtn.addEventListener("click", setHackerMode);
+// 3. Define what happens when event triggers function
+function setHackerMode() {
+    body.style.background = "black";
+    heading.style.fontFamily = "monospace";
+    heading.textContent  = "Error code 264";
+    description.style.color = "rgb(0, 255, 94)";
+    description.textContent = "YOU HAVE BEEN HACKED ):";
+    image.src = "warningsign.webp";
+}
+
+const sunsetBtn = document.querySelector("#sunset");
+sunsetBtn.addEventListener("click", setSunsetMode);
+function setSunsetMode() {
+    body.style.background = "linear-gradient(gold, lavender)";
+    heading.textContent = "Welcome to the view";
+    heading.style.color = "rgb(38, 62, 183)";
+    heading.style.fontFamily = "sunset heaven";
+    description.style.color = "rgb(38, 62, 183)";
+    description.textContent = "You have now been blessed with an amazing view!";
+    image.src = "sunset.webp";    
+}
+
+
+
+
+
+
+
 
